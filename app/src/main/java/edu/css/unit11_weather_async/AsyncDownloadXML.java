6f922ca322acc9e15ce53f0621bb1722d9e52ffe
briannagerold <bgerold@css.edu>
2019-04-07 11:50:36 -0500

    Assignment 11

--- app/src/main/java/edu/css/unit11_weather_async/AsyncDownloadXML.java
+++ app/src/main/java/edu/css/unit11_weather_async/AsyncDownloadXML.java
@@ -16,13 +16,22 @@ import java.util.Scanner;
 
 import static org.xmlpull.v1.XmlPullParser.TYPES;
 
+/**
+ * @author Tom Gibbons
+ * @version 1.0
+ * This class is used to get weather data Asynchronously
+ */
 public class AsyncDownloadXML extends AsyncTask<MainActivity, String, String> {
 
     MainActivity mainActivityLink;
 
-    //Implementation of AsyncTask used to download XML feed
-    // This method is run in a separate thread.  Do not do any UI stuff here.
-    // Calls onPostExecute when done and passes it the return value or String
+    /**
+     * Implementation of AsyncTask used to download XML feed
+     * This method is run in a separate thread.  Do not do any UI stuff here.
+     * Calls onPostExecute when done and passes it the return value or String
+     * @param new_actWeather
+     * @return
+     */
     @Override
     protected String doInBackground(MainActivity... new_actWeather) {
         try {
@@ -34,7 +43,9 @@ public class AsyncDownloadXML extends AsyncTask<MainActivity, String, String> {
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
             XmlPullParser xpp = factory.newPullParser();
 
-            String  weatherStrURL =  "http://api.openweathermap.org/data/2.5/weather?zip=55811,us&appid=5aa6c40803fbb300fe98c6728bdafce7&mode=xml&units=imperial";
+            String zip = mainActivityLink.getLocation();
+
+            String  weatherStrURL =  "http://api.openweathermap.org/data/2.5/weather?zip=" + zip +",us&appid=5aa6c40803fbb300fe98c6728bdafce7&mode=xml&units=imperial";
             URL weatherURL =  new URL(weatherStrURL);
             InputStream stream = weatherURL.openStream();
             xpp.setInput(stream, null);
@@ -42,7 +53,8 @@ public class AsyncDownloadXML extends AsyncTask<MainActivity, String, String> {
 
             String tempStr = "Updating...";			// Temperature Update String
             String windStr = "Updating...";			// Wind Update String
-            publishProgress(tempStr,windStr);
+            String visStr = "Updating...";          //Visibility Update String
+            publishProgress(tempStr,windStr,visStr);       //calls onProgressUpdate to update the user on the progress
 
             Log.v("== CIS 3334 ==","AsyncDownloadXML repeat until end of document arrives");
             while (eventType != XmlPullParser.END_DOCUMENT) {
@@ -55,46 +67,70 @@ public class AsyncDownloadXML extends AsyncTask<MainActivity, String, String> {
                     Log.v("== CIS 3334 ==","Start tag found with name = "+tag);
                     if (tag.equals("speed")){
                         // XML should look like: <speed value="11.41" name="Strong breeze"/>
-
-                        // ======= CIS 3334 add code here to process wing speed =======
+                        windStr = xpp.getAttributeValue(null, "value");
+                        Log.v("== CIS 3334 ==","Wind =" + windStr);
+                        publishProgress(tempStr,windStr, visStr);	// Update the display
 
                     }
                     if (tag.equals("temperature")){
                         // XML should look like: <temperature value="37.47" min="33.8" max="41" unit="fahrenheit"/>
                         tempStr = xpp.getAttributeValue(null, "value");
                         Log.v("== CIS 3334 ==","Temp =" + tempStr);
-                        publishProgress(tempStr,windStr);	// Update the display
+                        publishProgress(tempStr,windStr, visStr);	// Update the display
+                    }
+                    if (tag.equals("visibility")){
+                        // XML should look like: <temperature value="37.47" min="33.8" max="41" unit="fahrenheit"/>
+                        visStr = xpp.getAttributeValue(null, "value");
+                        Log.v("== CIS 3334 ==","Vis =" + visStr);
+                        publishProgress(tempStr,windStr,visStr);	// Update the display
                     }
                 }
                 eventType = xpp.next();
             }
-           return "Successfully updated weather";
+           return "Successfully updated weather"; //returns that is was successful
 
         } catch (IOException e) {
             Log.v("== CIS 3334 -- ERROR ==","AsyncDownloadXML doInBackground IOException");
             Log.v("== CIS 3334 -- ERROR ==",e.getMessage());
-            return(e.getMessage());
+            return(e.getMessage()); //returns error message
         } catch (XmlPullParserException e) {
             Log.v("== CIS 3334 -- ERROR ==","AsyncDownloadXML doInBackground XmlPullParserException");
             Log.v("== CIS 3334 -- ERROR ==",e.getMessage());
-            return(e.getMessage());
+            return(e.getMessage()); //returns error message
         }  catch (Exception e) {
             Log.v("== CIS 3334 -- ERROR ==","AsyncDownloadXML doInBackground Exception");
             Log.v("== CIS 3334 -- ERROR ==",e.getMessage());
-            return(e.getMessage());
+            return(e.getMessage()); //returns error message
         }
     }
 
+    /**
+     * gives an update to the mainActivity
+     * so the user knows what is going on.
+     * @param update
+     */
     @Override
     protected void onProgressUpdate(String... update) {
+        //logs the progress
         Log.v("== CIS 3334 ==","in onProgressUpdate");
+        //calls setTemp in the MainActivity and sends it the temp string that was sent
         mainActivityLink.setTemp(update[0]);
+        //calls setWing in the MainActiviy and sends it the wind string that was passed
         mainActivityLink.setWind(update[1]);
+        //calls setVis in the MainActivity and sends it the visibility that was passed
+        mainActivityLink.setVis(update[2]);
     }
 
+    /**
+     * When the task is over, send the
+     * result to the main activity
+     * @param result
+     */
     @Override
     protected void onPostExecute(String result) {
+        //logs that it is in onPostEsecute
         Log.v("== CIS 3334 ==", "in onPostExecute");
+        //calls setStatus in MainActivity and passes the result (success or error)
         mainActivityLink.setStatus(result);
     }
 
--- app/src/main/java/edu/css/unit11_weather_async/MainActivity.java
+++ app/src/main/java/edu/css/unit11_weather_async/MainActivity.java
@@ -12,14 +12,26 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 
+/**
+ * @author Tom Gibbons
+ * @author Brianna Gerold
+ * @version 2.0
+ * This class is the logic behind the screen
+ */
 public class MainActivity extends AppCompatActivity {
     EditText etLoc;
     EditText etTemp;
     EditText etWind;
     EditText etVis;
 
-    AsyncDownloadXML AsyncWeatherDownloader = new AsyncDownloadXML();
+    //new AsyncDownloadXML object
+    AsyncDownloadXML AsyncWeatherDownloader;
 
+    /**
+     * Called when the activity is starated
+     * sets up the text fields
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,26 +44,66 @@ public class MainActivity extends AppCompatActivity {
         etVis =  (EditText) findViewById(R.id.textVis);
     }
 
+    /**
+     * When the button is clicked this method
+     * is called. It starts the
+     * AsyncWeatherDownloader task
+     * @param v - the view
+     * @throws XmlPullParserException
+     * @throws URISyntaxException
+     * @throws IOException
+     */
     public void btnClick (View v) throws XmlPullParserException, URISyntaxException, IOException {
         // Download the weather asynchronously
+        AsyncWeatherDownloader = new AsyncDownloadXML();
         AsyncWeatherDownloader.execute(this);
     }
 
+    /**
+     * Sets the temp that is passed to
+     * the edit text field on the screen
+     * @param newTemp
+     */
     public void setTemp(String newTemp) {
         etTemp.setText(newTemp);
     }
 
+    /**
+     * Sets the wind that is passed to
+     * the edit text field on the screen
+     * @param newWind
+     */
     public void setWind(String newWind) {
         etWind.setText(newWind);
     }
 
+    /**
+     * Sets the visibility that is passed
+     * to the edit text field
+     * @param newVis
+     */
+    public void setVis(String newVis){
+        etVis.setText(newVis);
+    }
+
+    /**
+     * Returns the location the user
+     * has entered as a string
+     * @return
+     */
     public String getLocation() {
         return etLoc.getText().toString();
     }
 
-
+    /**
+     * Takes in a new status and
+     * displays it in a toast
+     * @param newStatus
+     */
     public void setStatus(String newStatus) {
+        //creates a toast with the new status
         Toast toast=Toast.makeText(getApplicationContext(), newStatus,Toast.LENGTH_LONG );
+        //displays the toast
         toast.show();
     }
 
